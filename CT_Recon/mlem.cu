5cfd1de4d7a1f23acec170aec1ff74882c31f0b1
hfan36 <hfan36@gmail.com>
2014-08-21 16:13:49 -0700

    read only feature did not work, but made writeparameterfile function output .info instead of .cfg

--- CT_Recon/mlem.cu
+++ CT_Recon/mlem.cu
@@ -18,26 +18,26 @@ void DisplayError(LPTSTR lpszFunction);
 
 int main()
 {
-	//cudaProfilerStart();
+	cudaProfilerStart();
 
-	//double totalTime;
-	//StopWatchInterface *timer;
-	//sdkCreateTimer(&timer);
-	//sdkResetTimer(&timer);
-	//sdkStartTimer(&timer);
+	double totalTime;
+	StopWatchInterface *timer;
+	sdkCreateTimer(&timer);
+	sdkResetTimer(&timer);
+	sdkStartTimer(&timer);
 	
 	//---------------- how to use siddon_recon class --------------------------------
-	//siddon_recon recon;
+	siddon_recon recon;
 	//recon.a1_FORWARD_PROJECTION("H:\\Visual Studio 2010\\CT_Recon\\CT_Recon", "CTParameters.h");
-	//recon.a0_RECON_MLEM("H:\\Visual Studio 2010\\CT_Recon\\CT_Recon\\", "CTParameters.h");
+	recon.a0_RECON_MLEM("H:\\Visual Studio 2010\\CT_Recon\\CT_Recon\\", "CTParameters.cfg");
 	//recon.a1_BACKWARD_PROJECTION("H:\\Visual Studio 2010\\CT_Recon\\CT_Recon", "MasterParameterFile.cfg", true);
 	//-------------------------------------------------------------------
 	
-	//sdkStopTimer(&timer);
-	//totalTime = sdkGetTimerValue(&timer)*1e-3;
-	//printf("calculation time = %f seconds \n", totalTime);
+	sdkStopTimer(&timer);
+	totalTime = sdkGetTimerValue(&timer)*1e-3;
+	printf("calculation time = %f seconds \n", totalTime);
 
-	//cudaProfilerStop();
+	cudaProfilerStop();
 
 
 	system("PAUSE");
